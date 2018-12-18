354213197b4b080ef61cec2c13684df8b0116d86
Robert Prus <robert.prus87@gmail.com>
2018-12-18 02:07:14 +0100

    Initial project has been added.

---
+++ src/main/java/hellspawn287/spring5_chuck_jokesapp/joke/JokeappApplication.java
@@ -0,0 +1,14 @@
+package hellspawn287.spring5_chuck_jokesapp.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+}
+
